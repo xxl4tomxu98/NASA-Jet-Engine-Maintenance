20470ec198efb12c4618a51a3ebee8c46e0b08f6
xxl4tomxu98 <tomxu@udel.edu>
2022-11-02 12:41:13 -0400

    classification1

--- EDA/dnn.py
+++ EDA/dnn.py
@@ -53,10 +53,7 @@ def one_dcnn(n_filters, kernel_size, input_array, initializer):
 
 '''
 Define the function for generating CNN braches(heads)
-
 '''
-
-
 def CNNBranch(n_filters, window_length, input_features,
               strides_len, kernel_size, n_conv_layer):
     inputs = Input(shape=(window_length, input_features), name='input_layer')
@@ -65,7 +62,6 @@ def CNNBranch(n_filters, window_length, input_features,
         x = Conv1D(filters=n_filters, kernel_size=kernel_size, padding='same')(x)
         x = BatchNormalization()(x)
         x = Activation('relu')(x)
-
     outputs = Flatten()(x)
     #     causalcnn = Model(inputs, outputs=[outputs])
     cnnbranch = Model(inputs, outputs=outputs)
@@ -79,15 +75,12 @@ def TD_CNNBranch(n_filters, window_length, n_window, input_features,
                             input_shape=(n_window, window_length, input_features)))
     cnn.add(TimeDistributed(BatchNormalization()))
     cnn.add(TimeDistributed(Activation('relu')))
-
     if n_conv_layer == 1:
         pass
-
     elif n_conv_layer == 2:
         cnn.add(TimeDistributed(Conv1D(filters=n_filters, kernel_size=kernel_size, padding='same', kernel_initializer=initializer)))
         cnn.add(TimeDistributed(BatchNormalization()))
         cnn.add(TimeDistributed(Activation('relu')))
-
     elif n_conv_layer == 3:
         cnn.add(TimeDistributed(Conv1D(filters=n_filters, kernel_size=kernel_size, padding='same', kernel_initializer=initializer)))
         cnn.add(TimeDistributed(BatchNormalization()))
@@ -95,7 +88,6 @@ def TD_CNNBranch(n_filters, window_length, n_window, input_features,
         cnn.add(TimeDistributed(Conv1D(filters=n_filters, kernel_size=kernel_size, padding='same', kernel_initializer=initializer)))
         cnn.add(TimeDistributed(BatchNormalization()))
         cnn.add(TimeDistributed(Activation('relu')))
-
     elif n_conv_layer == 4:
         cnn.add(TimeDistributed(Conv1D(filters=n_filters, kernel_size=kernel_size, padding='same', kernel_initializer=initializer)))
         cnn.add(TimeDistributed(BatchNormalization()))
@@ -106,10 +98,8 @@ def TD_CNNBranch(n_filters, window_length, n_window, input_features,
         cnn.add(TimeDistributed(Conv1D(filters=n_filters, kernel_size=kernel_size, padding='same', kernel_initializer=initializer)))
         cnn.add(TimeDistributed(BatchNormalization()))
         cnn.add(TimeDistributed(Activation('relu')))
-
     cnn.add(TimeDistributed(Flatten()))
     print(cnn.summary())
-
     return cnn
 
 
@@ -127,11 +117,9 @@ def CNNB(n_filters, lr, decay, loss,
                    activation='linear')(x)
         x = BatchNormalization()(x)
         x = Activation('relu')(x)
-
     # x = Dense(7, activation='relu', name='dense_layer')(x)
     outputs = Dense(3, activation='sigmoid', name='output_layer')(x)
     causalcnn = Model(inputs, outputs=[outputs])
-
     return causalcnn
 
 
@@ -139,15 +127,12 @@ def multi_head_cnn(sensor_input_model, n_filters, window_length, n_window,
                    input_features, strides_len, kernel_size, n_conv_layer, initializer):
     cnn_out_list = []
     cnn_branch_list = []
-
     for sensor_input in sensor_input_model:
         cnn_branch_temp = TD_CNNBranch(n_filters, window_length, n_window,
                                        input_features, strides_len, kernel_size, n_conv_layer, initializer)
         cnn_out_temp = cnn_branch_temp(sensor_input)
-
         cnn_branch_list.append(cnn_branch_temp)
         cnn_out_list.append(cnn_out_temp)
-
     return cnn_out_list, cnn_branch_list
 
 
@@ -156,9 +141,7 @@ def sensor_input_model(sensor_col, n_window, window_length, input_features):
     for sensor in sensor_col:
         input_temp = Input(shape=(n_window, window_length, input_features), name='%s' % sensor)
         sensor_input_model.append(input_temp)
-
     return sensor_input_model
-
 
 
 def cudnnlstm(sequence_length, nb_features, lstm1, lstm2, nb_out, initializer):
@@ -174,21 +157,14 @@ def cudnnlstm(sequence_length, nb_features, lstm1, lstm2, nb_out, initializer):
     model.add(Dropout(0.2))
     model.add(Dense(units=nb_out))
     model.add(Activation("linear"))
-
-
     return model
 
 
-
-def mlps(vec_len, h1, h2, h3, h4):
-    '''
-
-    '''
+def mlps(vec_len, h1, h2, h3, h4):   
     model = Sequential()
     model.add(Dense(h1, activation='relu', input_shape=(vec_len,)))
     model.add(Dense(h2, activation='relu'))
     model.add(Dense(h3, activation='relu'))
     model.add(Dense(h4, activation='relu'))
     model.add(Dense(1))
-
     return model
